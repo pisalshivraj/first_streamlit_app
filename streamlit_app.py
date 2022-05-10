b888f2d79fb961212e9e06153a3aecfbbe391e55
Shivraj Pisal <105319014+pisalshivraj@users.noreply.github.com>
2022-05-10 15:02:10 -0700

    Update streamlit_app.py
    Forgot to add .index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #adding multiselect for the users to pick the items 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
